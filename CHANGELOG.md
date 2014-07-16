e5aaa7ea961e13a36e5b07b25b009f861897a9e3
Brandon Mathis <brandon@imathis.com>
2014-07-16 16:58:58 -0500

    Fixed ink docs plugin

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+### 1.0.1 - 2014-07-16
+
+- Fixed Octopress Ink docs plugin.
+
 ### 1.0.0 - 2014-07-15
 
 - Initial release
--- lib/octopress-content-for/ink-plugin.rb
+++ lib/octopress-content-for/ink-plugin.rb
@@ -2,9 +2,9 @@ begin
   require 'octopress-ink'
 
   Octopress::Ink.add_plugin({
-    name:        'Include Tag',
+    name:        'Content For',
     assets_path: File.join(File.expand_path(File.dirname(__FILE__)), '../../assets' ),
-    description: "Test some plugins y'all"
+    description: "Add content_for and yield tags to Jekyll with conditional rendering and in-line filters."
   })
 rescue LoadError
 end
